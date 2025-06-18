dd73585fc4298baac4b63b061ddf968acab0d372
tanyastead <tatiana.stead@outlook.com>
2025-06-18 11:05:52 +0100

    populate_repository script WIP

--- database/populate_repository.py
+++ database/populate_repository.py
@@ -13,19 +13,27 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     'gene_exp_db', type=str, nargs='?',
     help="empty sqlite database (it should have the schema from repository.sql)",
-    default='./data/microdb.sqlite' ### Change!!
+    default='./repository.sqlite'
 )
 parser.add_argument(
-    'input_csv_path', type=str, nargs='?',
-    help="input CSV file containing gene expression data",
-    default='./data/microbial_growth_data.csv' ### Change!!
+    'input_csv_path', type=str,
+    help="input CSV file containing gene expression data"
+)
+parser.add_argument(
+    '-a', '--annotation_file',
+    help="specify the database will be loaded with an annotation file"
 )
 
 """ NOTE: at the moment have separate files for gene expression/molecular function
 Both kinds of files need to be inputted into the database - have separate arguments/functions for if this is the case?
+Can also include separate optional arguments to include fungal species and abiotic conditions?
 """
 
 args = parser.parse_args()
 
 # connect to database
 conn = sqlite3.connect(args.micro_db) # connect to sqlite3 ###NOTE: need to change path in args!!
+
+# Define dictionaries and sets to keep track of database input
+gene_id = set()
+conditions = set()
